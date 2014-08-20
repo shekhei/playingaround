83af33f8e11df09a71013c727bb80d3c78c4f939
wong shek hei <shekhei@gmail.com>
2014-08-20 18:28:27 +0800

    newest version

--- cachehit/cachehitobj.js
+++ cachehit/cachehitobj.js
@@ -3,7 +3,7 @@ function toCamelCase(str) {
 }
 FastObj = function(keys, maxSize) {
 	return (function(){
-		var createIndex = 0, template = [], length=0, typeSize = 2+(keys.length*2),arr = new Array(maxSize*typeSize);
+		var createIndex = 0, template = [], length=0, padding=2, typeSize = padding+(keys.length*2),arr = new Array(maxSize*typeSize);
 		template[0] = undefined;
 		template[1] = false; // used or not
 		for ( var i = 0; i < keys.length; i++ ) {
@@ -12,18 +12,24 @@ FastObj = function(keys, maxSize) {
 		var templateFuncs = [];
 		for ( var i = 0 ;i < keys.length; i++ ) {
 			templateFuncs[toCamelCase("get_"+keys[i])] = (function(i){ 
-				return function(){ return arr[this.index+i]}
-			})(i+1);
+				return function(){ 
+					return arr[this.index+i]
+				}
+			})(i+padding);
 			templateFuncs[toCamelCase("set_"+keys[i])] = (function(i){ 
-				return function(val){ arr[this.index+i] = val;}
-			})(i+1);
+				return function(val){ 
+					arr[this.index+i] = val;
+				}
+			})(i+padding);
 		}
-		var innerType = function() {
-			this.index = i;
+		var innerType = function(i) {
+			this.index = i*typeSize;
 			for ( var name in templateFuncs ) {
 				this[name] = templateFuncs[name];
 			}
+
 		}
+		function nextFunc(){return arr[this.index+typeSize];}
 		function create() {
 			var type = new innerType(createIndex);
 
@@ -32,14 +38,17 @@ FastObj = function(keys, maxSize) {
 				arr[pos+i] = template[i];
 			}
 			arr[pos] = type;
-						createIndex++;
+			createIndex++;
 			length++;
+			type.next = nextFunc;
 			return type;
 		}
 		return {
 			new: create,
 			getLength: function(){return length;},
-			get: function(i){return arr[i*typeSize];}
+			get: function(i){
+				return arr[i*typeSize];
+			}
 		};
 	})();
 
